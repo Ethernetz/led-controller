94bbd5eb38cdc21f04bc8b15815d5784bd875ec7
Ethernetz <Ethannetz@hotmail.com>
2020-10-03 21:47:39 -0400

    arduino changed ip :(

--- src/common/Leds.ts
+++ src/common/Leds.ts
@@ -14,7 +14,7 @@ export const storedLeds: Led[] = [
     {
         id: 0,
         name: "Wall",
-        ip: "10.0.0.28",
+        ip: "10.0.0.22",
         hex: "#ff0000",
         brightness: 20,
         on: true,
@@ -22,17 +22,25 @@ export const storedLeds: Led[] = [
     {
         id: 1,
         name: "Tube 1",
-        ip: "10.0.0.169",
+        ip: "10.0.0.166",
         hex: "#00ff00",
-        brightness: 30,
+        brightness: 20,
         on: true,
     },
     {
         id: 2,
         name: "Tube 2",
-        ip: "10.0.0.110",
+        ip: "10.0.0.109",
         hex: "#0000ff",
-        brightness: 40,
+        brightness: 20,
         on: true,
-    }
+    },
+    // {
+    //     id: 3,
+    //     name: "Bed",
+    //     ip: "10.0.0.187",
+    //     hex: "#00ffff",
+    //     brightness: 20,
+    //     on: true,
+    // }
 ]
